4911b3a5c1b5a37a720e0a1efe2d48ae29ac055d
Felipe Gil <felipe.gil@redspace.com>
2021-06-14 13:12:41 -0300

    skeleton

---
+++ SevenLittleWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenLittleWords
+//
+//  Created by Felipe Gil on 2021-06-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
